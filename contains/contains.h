4d3e779e813e672b0184a0885439fbac61deae81
mounawarmadani <mounawar.madani@hepl.be>
2018-11-18 22:23:35 +0100

    val retour précisée en commentaire

--- contains/contains.h
+++ contains/contains.h
@@ -1,4 +1,6 @@
-size_t contains(int vect[],size_t size,int elem) {
-	//Ecrire le code de la fonction
+int contains(int vect[],size_t size,int elem) {
+	// Ecrire le code de la fonction contains
+	// la fonction renvoie l'indice de la première occurence de l'élément si il est présent dans le vecteur
+	// la fonction renvoie -1 si l'élément n'est pas trouvé
 	return -1;
 }
